644ad60388234ce367a58d2b47cc663597a75048
Anna <annagiulia.musso@gmail.com>
2020-10-28 08:49:52 -0500

    more dom

---
+++ main.js
@@ -0,0 +1,115 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/dom.js":
+/*!********************!*\
+  !*** ./src/dom.js ***!
+  \********************/
+/*! namespace exports */
+/*! export createDomEle [provided] [no usage info] [missing usage info prevents renaming] */
+/*! export displayProject [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createDomEle\": () => /* binding */ createDomEle,\n/* harmony export */   \"displayProject\": () => /* binding */ displayProject\n/* harmony export */ });\n/* harmony import */ var _projects__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./projects */ \"./src/projects.js\");\n\n\nfunction createDomEle(parent, type, attributes, text = null) {\n    const parentContainer = document.querySelector(parent);\n    const element = document.createElement(type);\n    \n    if (attributes != null) {\n        for (let key in attributes) {\n            let value = attributes[key];\n            if (key != \"class\") {\n                element.setAttribute(key, value);                \n                } else {                     \n                   for (let i = 0; i < value.length; i++) {\n                        element.classList.add(value[i]);\n                   \n                    }       \n            \n            }\n            \n        };\n    }\n\n    if (text != null) {\n        element.textContent = text;\n    } else {\n        element.textContent = \"\";\n    }\n    \n    parentContainer.appendChild(element); \n}\n\nfunction addNewListener(target, event, action) {\n    const newTargets = document.querySelectorAll(target);\n    newTargets.forEach(target => \n        target.addEventListener(event, action));\n}\n\n//addNewListener(\"#new-pj-btn\", \"click\", alert(\"hello\"));\naddNewListener(\"#submit-pj-btn\", \"click\", () => {\n    (0,_projects__WEBPACK_IMPORTED_MODULE_0__.createNewPj)();\n    resetForm();\n});\n\naddNewListener(\"#reset-btn\", \"click\", resetForm);\n\n// const submitBtn = document.querySelector(\"#submit-pj-btn\");\n// submitBtn.addEventListener(\"click\", () => {\n//     console.log(submitBtn);\n// });\n function resetForm() {\n     const form = document.querySelector(\"form\");\n     form.reset();\n }\n\n function displayProject(project) {\n     createDomEle(\"#project-container\",\n     \"li\",\n     {class : [\"project\", project.category],   \n     id : project.title,     \n     });\n\n     //createProject\n }\n\n\n\n//# sourceURL=webpack://todoList/./src/dom.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom */ \"./src/dom.js\");\n\n\n\n//const container = document.querySelector(\"#project-container\")\n\n\n// format(new Date(2014, 1, 11), 'yyyy-MM-dd')\n// //=> '2014-02-11'\n\n// const dates = [\n//   new Date(1995, 6, 2),\n//   new Date(1987, 1, 11),\n//   new Date(1989, 6, 10),\n// ]\n// console.log(dates.sort(compareAsc));\n//=> [\n//   Wed Feb 11 1987 00:00:00,\n//   Mon Jul 10 1989 00:00:00,\n//   Sun Jul 02 1995 00:00:00\n// ]\n\n//# sourceURL=webpack://todoList/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/projects.js":
+/*!*************************!*\
+  !*** ./src/projects.js ***!
+  \*************************/
+/*! namespace exports */
+/*! export createNewPj [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createNewPj\": () => /* binding */ createNewPj\n/* harmony export */ });\n/* harmony import */ var _dom__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dom */ \"./src/dom.js\");\n\n\nconst projectFactory = (title, category, dueDate, priority, description) => {\n\n    return { title, category, dueDate, priority, description }\n}\n\nconst projectList = (() => {\n    const list = [];\n\n    return {list}\n})();\n\nfunction createNewPj() {\n    const title = document.querySelector(\"#pj-title-input\").value;\n    const dueDate = document.querySelector(\"#date-input\").value;\n    const category = document.querySelector(\"#category-input\").value;\n    const priority = document.querySelector(\"#priority-select\").value;\n    const description = document.querySelector(\"textarea\").value;\n\n    const newProject = projectFactory(title, category, dueDate, priority, description);\n    addPjToList(newProject);\n    (0,_dom__WEBPACK_IMPORTED_MODULE_0__.displayProject)(newProject);\n    console.log(projectList.list);\n}\n\n\n\nfunction addPjToList(project) {\n    const list = projectList.list;\n    list.push(project);\n}\n\n\n\n//# sourceURL=webpack://todoList/./src/projects.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
