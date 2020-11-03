0130fdeb8a379f40449ee1906fb321f848fdccd9
Anna <annagiulia.musso@gmail.com>
2020-11-03 11:17:47 -0600

    task display

--- src/dom.js
+++ src/dom.js
@@ -40,78 +40,130 @@ function addNewListener(target, event, action) {
 addNewListener("#reset-btn", "click", resetForm);
 
 function displayTask(task) {
+    const taskTitle = formatString(task.title);
+    // ctreate main div for each task
+    createTaskDiv(task, taskTitle);
+    createHeadDiv(task, taskTitle);
+    addHeadContent(task, taskTitle);
     
+    createExtraDiv(task, taskTitle);
+    addExtraContent(task, taskTitle);
+    console.log(task);
+}
+
+function createTaskDiv(task, taskTitle) {
     createDomEle("#tasks-container",
-    "li",
+    "div",
     {class : ["task", formatString(task.category)],   
-    id : formatString(task.title),     
+    id : `task-${taskTitle}`,     
     });
+}
 
-        createDomEle(`#${formatString(task.title)}`,
-        "div",
-        { class : ["task-details"],
-            id : `details-${formatString(task.title)}`
-        });
+function createHeadDiv(task, taskTitle) {
+    createDomEle(`#task-${taskTitle}`, 
+    "div",
+    { class : ["task-head"],
+        id : `head-${taskTitle}`
+    });
+}
 
-            createDomEle(`#details-${formatString(task.title)}`, 
-            "div",
-            { class : ["task-info"],
-                id : `info-${formatString(task.title)}`
-            });
+function addHeadContent(task, taskTitle) {
+    // append title to head
+    createDomEle(`#head-${taskTitle}`, 
+    "div",
+    { class : ["task-title"],
+        id : `title-${taskTitle}`
+    },
+    capitalize(task.title)  //text content
+    );
+    
+    //append due date
+    createDomEle(`#head-${taskTitle}`,
+    "span",
+    {class : ["dueDate-span"],
+    id : `dueDate-span-${taskTitle}` }
+    );
 
-                createDomEle(`#info-${formatString(task.title)}`, 
-                "span",
-                { class : ["task-title"],
-                    id : `title-${formatString(task.title)}`
-                },
-                capitalize(task.title)  //text content
-                );
-                
-                if (task.dueDate != "") {
-                    createDomEle(`#info-${formatString(task.title)}`,
-                    "span",
-                    {class : ["dueDate-span"],
-                    id : `dueDate-span-${formatString(task.title)}` }
-                    );
+    if (task.dueDate != "") {
+        createDomEle(`#dueDate-span-${taskTitle}`,
+        "span",
+        {class : ["dueDate-icon-span"],
+        id : `dueDate-icon-span-${taskTitle}` },
+        );
 
-                    createDomEle(`#dueDate-span-${formatString(task.title)}`,
-                    "span",
-                    {class : ["dueDate-icon-span"],
-                    id : `dueDate-icon-span-${task.title}` },
-                    );
+        createDomEle(`#dueDate-icon-span-${taskTitle}`,
+        "i",
+        {class : ["fa", "fa-calendar"]},
+        );
+    }          
+    createDomEle(`#dueDate-span-${taskTitle}`,
+    "span",
+    {id : `dueDate-text-${taskTitle}` },
+    task.dueDate);
 
-                    createDomEle(`#dueDate-icon-span-${task.title}`,
-                    "i",
-                    {class : ["fa", "fa-calendar"]},
-                    );
+    //append priority
+    createDomEle(`#head-${taskTitle}`,
+    "span",
+    {class : ["priority-span"],
+    id : `priority-span-${taskTitle}` }
+    );
 
-                    createDomEle(`#dueDate-span-${formatString(task.title)}`,
-                    "span",
-                    {id : `dueDate-text-${task.title}` },
-                    task.dueDate);
+    if (task.priority === "high") {
+        createDomEle(`#priority-span-${taskTitle}`,
+        "span",
+        {class : ["priority-icon-span"],
+        id : `priority-icon-span-${taskTitle}` },
+        );
 
-                    // ADD CATEGORY AND PRIORITY FIELDS
-                    // SPLIT METHOD IN SMALLER METHODS
-                }
-                
+        createDomEle(`#priority-icon-span-${taskTitle}`,
+        "i",
+        {class : ["fa", "fa-exclamation-triangle"]},
+        );
+    }  
 
-                console.log(task.dueDate);
+/// FIX ALIGNMENT RIGHT SIDE TASK DETAILS
+    
+        
+}
 
-    // if (task.category != "") {
-    //     createDomEle(`#details-${task.title}`,
-    //     "div",
-    //     {id : `category-${task.title}` },
-    //     task.category);
-    // }
- 
+function addExtraContent(task, taskTitle) {
+    //append task category to extra
+    if (task.category != "") {
+        createDomEle(`#extra-${taskTitle}`, 
+        "span",
+        { class : ["category-span"],
+            id : `category-span-${taskTitle}` },
+            capitalize(task.category));
+    } else {
+        createDomEle(`#extra-${taskTitle}`, 
+        "span",
+        { class : ["category-span"],
+            id : `category-span-${taskTitle}` },
+            "");
+    }
+    //append task description to extra
+    if (task.description != "") {
+        
+        createDomEle(`#extra-${taskTitle}`, 
+        "span",
+        { class : ["description-span"],
+            id : `description-span-${taskTitle}` },
+            task.description);
+    }    
+}
 
-    //  displayTaskInfo(task);
+function createExtraDiv(task, taskTitle) {
+    createDomEle(`#task-${taskTitle}`, 
+    "div",
+    { class : ["task-extra"],
+        id : `extra-${taskTitle}`
+    });
 }
 
 function formatString(string) {
     if (string != "") {
         
-        return string.split(' ').join('-');
+        return string.split(/\W+/).join('');
     } else { return ""};
   
 }
